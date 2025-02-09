6e3a5b93167228ca993f631f514cec9645a50731
willhamilton <willhamilton1991@gmail.com>
2025-02-09 22:03:06 +0200

    !U Tests grouped into different classes (positive & negative)

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,10 +14,10 @@ def tag_randomizer():
     content = response.json()
 
     for i in range(len(list(content))):
-        list_of_tags += list(content.values())[i]  # Is there something wrong about it https://www.geeksforgeeks.org/python-select-random-value-from-a-list/?
+        list_of_tags += list(content.values())[
+            i]  # Is there something wrong about it https://www.geeksforgeeks.org/python-select-random-value-from-a-list/?
 
     return random.choice(list_of_tags)
-
 
 def tags_comparer(tag, content):
     for element in content['images'][0]['tags']:
@@ -25,83 +25,81 @@ def tags_comparer(tag, content):
             return True
     return False
 
+class TestCasesPositive:
+    def test_get_search_random(self):
 
-def test_get_search_random():
+        headers = {'Authorization': auth_token}
 
-    headers = {'Authorization': auth_token}
-
-    response = requests.get(url=url, headers=headers)
-    assert response.status_code == 200
-
-    content = response.json()
-    assert content is not None
-    assert len(content) == 1 # Do we need to check length of content?
-
-    assert len(content['images']) == 1
-    assert len(content['images'][0]) == 16
-
-    assert isinstance(content['images'][0]['signature'], str)
-    assert isinstance(content['images'][0]['extension'], str)
-    assert isinstance(content['images'][0]['image_id'], int)
-    assert isinstance(content['images'][0]['favorites'], int)
-    assert isinstance(content['images'][0]['dominant_color'], str)
-    assert isinstance(content['images'][0]['source'], str)
-    assert isinstance(content['images'][0]['artist'], dict) or content['images'][0]['artist'] is None # TODO: Need to investigate behaviour here..
-    assert isinstance(content['images'][0]['uploaded_at'], str)
-    assert (content['images'][0]['liked_at'] is None) or (isinstance(content['images'][0]['liked_at'], str))  # TODO: Need to investigate behaviour here..
-    assert isinstance(content['images'][0]['is_nsfw'], bool)
-    assert isinstance(content['images'][0]['width'], int)
-    assert isinstance(content['images'][0]['height'], int)
-    assert isinstance(content['images'][0]['byte_size'], int)
-    assert isinstance(content['images'][0]['url'], str)
-    assert isinstance(content['images'][0]['preview_url'], str)
-
-
-def test_search_is_nsfw_false_by_default():
-    headers = {'Authorization': auth_token}
-
-    for test in range(5):
         response = requests.get(url=url, headers=headers)
         assert response.status_code == 200
 
         content = response.json()
-        assert content['images'][0]['is_nsfw'] == False
+        assert content is not None
+        assert len(content) == 1 # Do we need to check length of content?
+
+        assert len(content['images']) == 1
+        assert len(content['images'][0]) == 16
+
+        assert isinstance(content['images'][0]['signature'], str)
+        assert isinstance(content['images'][0]['extension'], str)
+        assert isinstance(content['images'][0]['image_id'], int)
+        assert isinstance(content['images'][0]['favorites'], int)
+        assert isinstance(content['images'][0]['dominant_color'], str)
+        assert isinstance(content['images'][0]['source'], str)
+        assert isinstance(content['images'][0]['artist'], dict) or content['images'][0]['artist'] is None # TODO: Need to investigate behaviour here..
+        assert isinstance(content['images'][0]['uploaded_at'], str)
+        assert (content['images'][0]['liked_at'] is None) or (isinstance(content['images'][0]['liked_at'], str))  # TODO: Need to investigate behaviour here..
+        assert isinstance(content['images'][0]['is_nsfw'], bool)
+        assert isinstance(content['images'][0]['width'], int)
+        assert isinstance(content['images'][0]['height'], int)
+        assert isinstance(content['images'][0]['byte_size'], int)
+        assert isinstance(content['images'][0]['url'], str)
+        assert isinstance(content['images'][0]['preview_url'], str)
+
+    def test_search_is_nsfw_false_by_default(self):
+        headers = {'Authorization': auth_token}
+
+        for test in range(5):
+            response = requests.get(url=url, headers=headers)
+            assert response.status_code == 200
+
+            content = response.json()
+            assert content['images'][0]['is_nsfw'] == False
+
+    def test_get_search_single_tag_included(self):
+        random_tag = tag_randomizer()
+
+        headers = {'Authorization': auth_token}
+        response = requests.get(url=f"https://api.waifu.im/search/?included_tags={random_tag}", headers=headers)
+        content = response.json()
+
+        assert tags_comparer(tag=random_tag, content=content), 'Tags are not equal.'
+
+    @pytest.mark.skip(reason="BUG #3: Get search does not return picture with multiple searched tags. Repro rate 50%")
+    def test_get_search_multiple_tag_included(self):
+        random_tag_1 = tag_randomizer()
+        random_tag_2 = tag_randomizer()
+
+        response = requests.get(url=f"https://api.waifu.im/search/?included_tags={random_tag_1}&{random_tag_2}")
+        content = response.json()
+
+        assert tags_comparer(random_tag_1, content)
+        assert tags_comparer(random_tag_2, content)
 
 
-def test_get_search_single_tag_included():
-    random_tag = tag_randomizer()
-
-    headers = {'Authorization': auth_token}
-    response = requests.get(url=f"https://api.waifu.im/search/?included_tags={random_tag}", headers=headers)
-    content = response.json()
-
-    assert tags_comparer(random_tag, content), 'Tags are not equal.'
+class TestCasesNegative:
+    @pytest.mark.skip(reason="BUG #2: Request does not require Bearer token")
+    def test_get_search_random_without_header(self):
+        response = requests.get(url="https://api.waifu.im/search")
+        assert response.status_code == 401
 
 
-@pytest.mark.skip(reason="BUG #3: Get search does not return picture with multiple searched tags. Repro rate 50%")
-def test_get_search_multiple_tag_included():
-    random_tag_1 = tag_randomizer()
-    random_tag_2 = tag_randomizer()
+    @pytest.mark.skip(reason="BUG #4: Get search returns random tag while to similar included tags requested added")
+    def test_get_search_same_multiple_tag_included(self):
+        random_tag = tag_randomizer()
 
-    response = requests.get(url=f"https://api.waifu.im/search/?included_tags={random_tag_1}&{random_tag_2}")
-    content = response.json()
+        response = requests.get(url=f"https://api.waifu.im/search/?{random_tag}&{random_tag}")
+        content = response.json()
 
-    assert tags_comparer(random_tag_1, content)
-    assert tags_comparer(random_tag_2, content)
-
-
-@pytest.mark.skip(reason="BUG #4: Get search returns random tag while to similar included tags requested added")
-def test_get_search_same_multiple_tag_included():
-    random_tag = tag_randomizer()
-
-    response = requests.get(url=f"https://api.waifu.im/search/?{random_tag}&{random_tag}")
-    content = response.json()
-
-    assert response.status_code == 200, 'Wrong status code returned.'
-    assert tags_comparer(tag=random_tag, content=content)
-
-
-@pytest.mark.skip(reason="BUG #2: Request does not require Bearer token")
-def test_get_search_random_without_header():
-    response = requests.get(url="https://api.waifu.im/search")
-    assert response.status_code == 401
+        assert response.status_code == 200, 'Wrong status code returned.'
+        assert tags_comparer(tag=random_tag, content=content)
