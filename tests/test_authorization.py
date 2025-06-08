ebba4fd6771d5820c031d8baa7542c61c86460e7
MasonTwoK <willhamilton1991@gmail.com>
2025-06-08 13:13:34 +0300

    !U set discord id as environment variable

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,11 +2,12 @@
 
 import pytest
 import requests
+import os
 
 
 def test_get_authorization():
     # How to use ID: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID
-    # discord_id:
+    discord_id = os.environ['discord_id']
     params = {'user_id': discord_id, 'permissions': ['view_favorites']}
     response = requests.get(url="https://www.waifu.im/authorization", params=params)
 
