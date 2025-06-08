87487178efa4151604ab0e50a561b6444aa376aa
MasonTwoK <willhamilton1991@gmail.com>
2025-06-08 20:59:24 +0300

    !U updated auth with Bearer

--- tests/test_favorites.py
+++ tests/test_favorites.py
@@ -5,14 +5,15 @@ import requests
 import os
 
 
-@pytest.mark.skip(reason="TBD. Q: How to put Bearer Token to requests.get()")
+@pytest.mark.skip(reason="TBD. Q: How to put pictures in favorite?")
 def test_get_favorites():
 
     auth_token = os.environ.get('auth_token')  # How to get a token https://docs.waifu.im/authorization
     headers = {
         'Accept-Version': 'v5',
-        'Authorization': auth_token
+        'Authorization': f'Bearer {auth_token}'
     }
 
     response = requests.get(url="https://api.waifu.im/fav", headers=headers)
-    assert response.status_code == 200
+    assert response.status_code == 404
+    print()
