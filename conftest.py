2f9362ccf743e448b2c08976cfce546d54d76825
MasonTwoK <willhamilton1991@gmail.com>
2025-06-30 13:00:30 +0300

    !U added method request_get_tags_query_full_false_response

--- conftest.py
+++ conftest.py
@@ -6,3 +6,9 @@ import requests
 def request_get_tags_response():
     response = requests.get(url="https://api.waifu.im/tags")
     yield response
+
+
+@pytest.fixture(scope="class")
+def request_get_tags_query_full_false_response():
+    response = requests.get(url=f"https://api.waifu.im/tags?full=false")
+    yield response
