6aa92d699ce47cdbc9e73ee1b7cf3ed75ab46476
MasonTwoK <willhamilton1991@gmail.com>
2025-07-02 09:19:23 +0300

    !U rework conftest.py methods due to Pablo`s review

--- conftest.py
+++ conftest.py
@@ -5,10 +5,23 @@ import requests
 @pytest.fixture(scope="class")
 def request_get_tags_response():
     response = requests.get(url="https://api.waifu.im/tags")
-    yield response
+    data = response.json()
+
+    yield response.status_code, data
 
 
 @pytest.fixture(scope="class")
 def request_get_tags_query_full_false_response():
-    response = requests.get(url=f"https://api.waifu.im/tags?full=false")
-    yield response
+    response = requests.get(url="https://api.waifu.im/tags?full=false")
+    data = response.json()
+
+    yield response.status_code, data
+
+
+# Чи можна прокинути зміну param= зі значеннями False/True щоб юзати лише 1 метод?
+@pytest.fixture(scope="class")
+def request_get_tags_query_full_true_response():
+    response = requests.get(url="https://api.waifu.im/tags?full=false")
+    data = response.json()
+
+    yield response.status_code, data
