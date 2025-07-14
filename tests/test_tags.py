f45ae1b0bca61b56f2037977f98e83e0eb218db3
MasonTwoK <willhamilton1991@gmail.com>
2025-07-14 20:55:29 +0300

    !U small refactor & paratramization of test_get_tags_query_full_true_amount_of_tags_in_group & test_get_tags_query_full_true_contains_info

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -17,7 +17,8 @@ class TestGetTags:
     @pytest.mark.positive
     @pytest.mark.status_code
     def test_get_tags_status_code(self, request_get_tags_response):
-        assert request_get_tags_response.status_code == 200, "Status code is not 200"
+        assert request_get_tags_response.status_code == 200, \
+            f"Status code {request_get_tags_response.status_code} is not 200"
 
     @pytest.mark.tags
     @pytest.mark.positive
@@ -69,7 +70,8 @@ class TestGetTagsFullInfoFalse:
     @pytest.mark.tags
     @pytest.mark.positive
     def test_get_tags_query_full_false_status_code(self, request_get_tags_query_full_false_response):
-        assert request_get_tags_query_full_false_response.status_code == 200, "Status code is not 200"
+        assert request_get_tags_query_full_false_response.status_code == 200, \
+            f"Status code {request_get_tags_query_full_false_response.status_code} is not 200"
 
     @pytest.mark.tags
     @pytest.mark.positive
@@ -90,7 +92,8 @@ class TestGetTagsFullInfoFalse:
     ])
     def test_get_tags_query_full_false_in_group_amount(self,
                                                        request_get_tags_query_full_false_response, group_name, amount):
-        assert len(request_get_tags_query_full_false_response.data[group_name]) == amount, 'Tags group amount is wrong'
+        assert len(request_get_tags_query_full_false_response.data[group_name]) == amount, \
+            f'Tags group {group_name} amount is wrong'
 
     @pytest.mark.tags
     @pytest.mark.positive
@@ -103,7 +106,8 @@ class TestGetTagsFullInfoFalse:
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.parametrize("nsfw_tag", nsfw_tags)
-    def test_get_tags_query_full_false_contains_in_nsfw(self, request_get_tags_query_full_false_response, nsfw_tag):
+    def test_get_tags_query_full_false_contains_in_nsfw(self,
+                                                        request_get_tags_query_full_false_response, nsfw_tag):
         assert nsfw_tag in request_get_tags_query_full_false_response.data['nsfw'], \
             f'{nsfw_tag} tag is not present in nsfw group'
 
@@ -117,7 +121,8 @@ class TestGetTagsFullInfoTrue:
     @pytest.mark.tags
     @pytest.mark.positive
     def test_get_tags_query_full_true_status_code(self, request_get_tags_query_full_true_response):
-        assert request_get_tags_query_full_true_response.status_code == 200
+        assert request_get_tags_query_full_true_response.status_code == 200, \
+            f'Status code {request_get_tags_query_full_true_response.status_code} is not 200'
 
     @pytest.mark.tags
     @pytest.mark.positive
@@ -132,70 +137,64 @@ class TestGetTagsFullInfoTrue:
 
     @pytest.mark.tags
     @pytest.mark.positive
-    @pytest.mark.parametrize("tag_id, name, is_nsfw, description", [
-        pytest.param(1, 'ass', True, 'Girls with a large butt. ', id='ass'),
-        pytest.param(2, 'ecchi', True, "Slightly explicit sexual content. Show full to partial nudity. "
-                                       "Doesn't show any genital.", id='ecchi'),
-        pytest.param(3, 'ero', True, 'Any kind of erotic content, basically any nsfw image.', id='ero'),
-        pytest.param(4, 'hentai', True, 'Explicit sexual content.', id='hentai'),
-        pytest.param(6, 'milf', True, 'A sexually attractive middle-aged woman.', id='milf'),
-        pytest.param(8, 'oral', True, 'Oral sex content.', id='oral'),
-        pytest.param(9, 'paizuri', True, 'A subcategory of hentai that involves breast sex, '
-                                         'also known as titty fucking.', id='paizuri')
+    @pytest.mark.parametrize("tag_id, name, is_nsfw, description, group_name", [
+        pytest.param(1, 'ass', True, 'Girls with a large butt. ', 'nsfw', id='ass'),
+        pytest.param(2, 'ecchi', True, "Slightly explicit sexual content. Show full to partial nudity. Doesn't show "
+                                       "any genital.", 'nsfw', id='ecchi'),
+        pytest.param(3, 'ero', True, 'Any kind of erotic content, basically any nsfw image.', 'nsfw', id='ero'),
+        pytest.param(4, 'hentai', True, 'Explicit sexual content.', 'nsfw', id='hentai'),
+        pytest.param(5, "marin-kitagawa", False,
+                     "One of two main protagonists (alongside Wakana Gojo) in the anime and manga series My Dress-Up "
+                     "Darling.", 'versatile', id="marin-kitagawa"),
+        pytest.param(6, 'milf', True, 'A sexually attractive middle-aged woman.', 'nsfw', id='milf'),
+        pytest.param(7, "oppai", False, "Girls with large breasts", 'versatile', id="oppai"),
+        pytest.param(8, 'oral', True, 'Oral sex content.', 'nsfw', id='oral'),
+        pytest.param(9, 'paizuri', True, 'A subcategory of hentai that involves breast sex, also known as titty '
+                     'fucking.', 'nsfw', id='paizuri'),
+        pytest.param(10, "selfies", False, "A photo-like image of a waifu.", 'versatile', id="selfies"),
+        pytest.param(11, "uniform", False, "Girls wearing any kind of uniform, cosplay etc... ", 'versatile',
+                     id="uniform"),
+        pytest.param(12, 'waifu', False, "A female anime/manga character.", 'versatile', id='waifu'),
+        pytest.param(13, 'maid', False, "Cute womans or girl employed to do domestic work in their working uniform.",
+                     'versatile', id='maid'),
+        pytest.param(14, "mori-calliope", False,
+                     "Mori Calliope is an English Virtual YouTuber (VTuber) associated with hololive as part of its "
+                     "first-generation English branch of Vtubers.", 'versatile', id="mori-calliope"),
+        pytest.param(15, "raiden-shogun", False,
+                     "Genshin Impact's Raiden Shogun is a fierce lady in the Genshin ranks.", 'versatile',
+                     id="raiden-shogun"),
+        pytest.param(17, "kamisato-ayaka", False, "Kamisato Ayaka is a playable Cryo character in Genshin Impact.",
+                     'versatile', id="kamisato-ayaka")
     ])
-    def test_get_tags_query_full_true_contains_info_for_nsfw_tags(self, request_get_tags_query_full_true_response,
-                                                                  tag_id, name, is_nsfw, description):
-        assert tag_full_info_comparer(request_get_tags_query_full_true_response.data['nsfw'],
+    def test_get_tags_query_full_true_contains_info(self, tag_id, name, is_nsfw, description, group_name,
+                                                    request_get_tags_query_full_true_response):
+        assert tag_full_info_comparer(request_get_tags_query_full_true_response.data[group_name],
                                       tag_id, name, is_nsfw, description), f"{name} tag has incorrect full info"
 
     @pytest.mark.tags
     @pytest.mark.positive
-    @pytest.mark.parametrize("tag_id, name, is_nsfw, description", [
-        pytest.param(5, "marin-kitagawa", False, "One of two main protagonists (alongside Wakana Gojo) in the anime "
-                                                 "and manga series My Dress-Up Darling.", id="marin-kitagawa"),
-        pytest.param(7, "oppai", False, "Girls with large breasts", id="oppai"),
-        pytest.param(10, "selfies", False, "A photo-like image of a waifu.", id="selfies"),
-        pytest.param(11, "uniform", False, "Girls wearing any kind of uniform, cosplay etc... ", id="uniform"),
-        pytest.param(12, 'waifu', False, "A female anime/manga character.", id='waifu'),
-        pytest.param(13, 'maid', False, "Cute womans or girl employed to do domestic work in their working uniform.",
-                     id='maid'),
-        pytest.param(14, "mori-calliope", False,
-                     "Mori Calliope is an English Virtual YouTuber (VTuber) associated with hololive as part of its "
-                     "first-generation English branch of Vtubers.", id="mori-calliope"),
-        pytest.param(15, "raiden-shogun", False,
-                     "Genshin Impact's Raiden Shogun is a fierce lady in the Genshin ranks.", id="raiden-shogun"),
-        pytest.param(17, "kamisato-ayaka", False, "Kamisato Ayaka is a playable Cryo character in Genshin Impact.",
-                     id="kamisato-ayaka")
+    @pytest.mark.parametrize("group_tag, amount", [
+        pytest.param('nsfw',  7, id='nsfw'),
+        pytest.param('versatile', 9, id='versatile')
     ])
-    def test_get_query_full_true_contains_info_for_versatile_tags(self, request_get_tags_query_full_true_response,
-                                                                  tag_id, name, is_nsfw, description):
-        assert tag_full_info_comparer(
-            request_get_tags_query_full_true_response.data['versatile'], tag_id, name, is_nsfw, description), \
-            f'Tag {name} full info is wrong'
-
-    @pytest.mark.tags
-    @pytest.mark.positive
-    def test_get_tags_query_full_true_amount_of_nsfw_tags(self, request_get_tags_query_full_true_response):
-        assert len(request_get_tags_query_full_true_response.data['nsfw']) == 7, 'Amount of nsfw tags is not 7'
-
-    @pytest.mark.tags
-    @pytest.mark.positive
-    def test_tags_query_full_true_amount_of_versatile_tags(self, request_get_tags_query_full_true_response):
-        assert len(request_get_tags_query_full_true_response.data['versatile']) == 9, \
-            'Amount of versatile tags is not 9'
+    def test_get_tags_query_full_true_amount_of_tags_in_group(self, request_get_tags_query_full_true_response,
+                                                              group_tag, amount):
+        assert len(request_get_tags_query_full_true_response.data[group_tag]) == amount, \
+            f'Amount of {group_tag} tags is not {amount}'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.parametrize("nsfw_tag", nsfw_tags)
-    def test_get_tags_query_full_true_nsfw_tags_info_size(self, request_get_tags_query_full_true_response, nsfw_tag):
+    def test_get_tags_query_full_true_nsfw_tags_info_size(self, nsfw_tag,
+                                                          request_get_tags_query_full_true_response):
         assert len(tag_full_info_provider(nsfw_tag, request_get_tags_query_full_true_response.data['nsfw'])) == 4, \
             f"{nsfw_tag} info fields don't equal 4"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.parametrize("versatile_tag", versatile_tags)
-    def test_get_tags_query_full_true_versatile_tags_info_size(self, request_get_tags_query_full_true_response,
-                                                               versatile_tag):
+    def test_get_tags_query_full_true_versatile_tags_info_size(self, versatile_tag,
+                                                               request_get_tags_query_full_true_response):
         assert len(tag_full_info_provider(
             versatile_tag, request_get_tags_query_full_true_response.data['versatile'])) == 4, \
             f"{versatile_tag} info fields don't equal 4"
