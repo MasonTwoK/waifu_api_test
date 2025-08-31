37619b2a8ce38fe23a289e0a644dc7f3d38a2f68
MasonTwoK <willhamilton1991@gmail.com>
2025-08-31 09:05:35 +0300

    !U permission parameter reverted to 'view_favorites'

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -11,7 +11,7 @@ load_dotenv()
 def test_get_authorization():
     # How to get ID: https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID
     discord_id = os.environ['discord_id']
-    params = {'user_id': discord_id, 'permissions': ['report']}
+    params = {'user_id': discord_id, 'permissions': ['view_favorites']}
     response = requests.get(url="https://www.waifu.im/authorization", params=params)
 
     assert response.status_code == 200
