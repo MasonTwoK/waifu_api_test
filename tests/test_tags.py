d33b13c340f276db827713bd45019116f7339207
MasonTwoK <willhamilton1991@gmail.com>
2025-09-11 19:56:09 +0300

    !U unified method request_get_tags_query_full in tests TestGetTagsQueryFullFalse & TestGetTagsQueryFullTrue

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -59,135 +59,135 @@ class TestGetTags:
 
 @pytest.mark.tags
 @pytest.mark.positive
+@pytest.mark.query_param('False')
 class TestGetTagsQueryFullFalse:
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.status_code
-    def test_get_tags_query_full_false_status_code(self, request_get_tags_query_full_false):
-        assert request_get_tags_query_full_false.status_code == 200, \
-            f"Status code {request_get_tags_query_full_false.status_code} is not 200"
+    def test_get_tags_query_full_false_status_code(self, request_get_tags_query_full):
+        assert request_get_tags_query_full.status_code == 200, \
+            f"Status code {request_get_tags_query_full.status_code} is not 200"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
-    def test_get_tags_query_full_false_groups_amount(self, request_get_tags_query_full_false):
-        assert len(request_get_tags_query_full_false.data) == 2, 'Tags group amount is wrong'
+    def test_get_tags_query_full_false_groups_amount(self, request_get_tags_query_full):
+        assert len(request_get_tags_query_full.data) == 2, 'Tags group amount is wrong'
 
     # Maybe we don't need this test?
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("group_name", tags_groups)
-    def test_get_tags_query_full_false_groups_presence(self, request_get_tags_query_full_false, group_name):
-        assert group_name in request_get_tags_query_full_false.data, f'{group_name} tags group is missing'
+    def test_get_tags_query_full_false_groups_presence(self, request_get_tags_query_full, group_name):
+        assert group_name in request_get_tags_query_full.data, f'{group_name} tags group is missing'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("group_name, amount", tags_in_group_amount)
-    def test_get_tags_query_full_false_in_group_amount(self, request_get_tags_query_full_false, group_name, amount):
-        assert len(request_get_tags_query_full_false.data[group_name]) == amount, \
+    def test_get_tags_query_full_false_in_group_amount(self, request_get_tags_query_full, group_name, amount):
+        assert len(request_get_tags_query_full.data[group_name]) == amount, \
             f'Tags group {group_name} amount is wrong'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("versatile_tag", versatile_tags)
-    def test_get_tags_query_full_false_contains_in_versatile(self, request_get_tags_query_full_false, versatile_tag):
-        assert versatile_tag in request_get_tags_query_full_false.data['versatile'], \
+    def test_get_tags_query_full_false_contains_in_versatile(self, request_get_tags_query_full, versatile_tag):
+        assert versatile_tag in request_get_tags_query_full.data['versatile'], \
             f'{versatile_tag} tag is not present in versatile group'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("nsfw_tag", nsfw_tags)
-    def test_get_tags_query_full_false_contains_in_nsfw(self, request_get_tags_query_full_false, nsfw_tag):
-        assert nsfw_tag in request_get_tags_query_full_false.data['nsfw'], \
+    def test_get_tags_query_full_false_contains_in_nsfw(self, request_get_tags_query_full, nsfw_tag):
+        assert nsfw_tag in request_get_tags_query_full.data['nsfw'], \
             f'{nsfw_tag} tag is not present in nsfw group'
 
 
 @pytest.mark.tags
 @pytest.mark.positive
+@pytest.mark.query_param('True')
 class TestGetTagsQueryFullTrue:
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.status_code
-    def test_get_tags_query_full_true_status_code(self, request_get_tags_query_full_true):
-        assert request_get_tags_query_full_true.status_code == 200, \
-            f'Status code {request_get_tags_query_full_true.status_code} is not 200'
+    def test_get_tags_query_full_true_status_code(self, request_get_tags_query_full):
+        assert request_get_tags_query_full.status_code == 200, \
+            f'Status code {request_get_tags_query_full.status_code} is not 200'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
-    def test_get_tags_query_full_true_groups_amount(self, request_get_tags_query_full_true):
-        assert len(request_get_tags_query_full_true.data) == 2, 'Tags group amount is wrong'
+    def test_get_tags_query_full_true_groups_amount(self, request_get_tags_query_full):
+        assert len(request_get_tags_query_full.data) == 2, 'Tags group amount is wrong'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("group_name", tags_groups)
-    def test_get_tags_query_full_true_groups_presence(self, request_get_tags_query_full_true, group_name):
-        assert group_name in request_get_tags_query_full_true.data, f'{group_name} is not present'
+    def test_get_tags_query_full_true_groups_presence(self, request_get_tags_query_full, group_name):
+        assert group_name in request_get_tags_query_full.data, f'{group_name} is not present'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("group_tag, amount", tags_in_group_amount)
-    def test_get_tags_query_full_true_amount_of_tags_in_group(self, request_get_tags_query_full_true, group_tag, amount):
-        assert len(request_get_tags_query_full_true.data[group_tag]) == amount, \
-            f'Amount of {group_tag} tags is not {amount}'
+    def test_get_tags_query_full_true_amount_of_tags_in_group(self, request_get_tags_query_full, group_tag, amount):
+        assert len(request_get_tags_query_full.data[group_tag]) == amount, f'Amount of {group_tag} tags is not {amount}'
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_full_info", tags_full_info)
-    def test_get_tags_query_full_true_tags_info_size(self, tag_full_info, request_get_tags_query_full_true):
-        assert len(tag_full_info_provider(request_get_tags_query_full_true.data, tag_full_info)) == 4, \
+    def test_get_tags_query_full_true_tags_info_size(self, tag_full_info, request_get_tags_query_full):
+        assert len(tag_full_info_provider(request_get_tags_query_full.data, tag_full_info)) == 4, \
             f"{tag_full_info['name']} info fields don't equal 4"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_name", tag_full_info_tag_names)
-    def test_tags_query_full_true_contains_info_name(self, request_get_tags_query_full_true, tag_name):
-        assert tag_full_info_tag_name_provider(request_get_tags_query_full_true.data, tag_name), \
+    def test_tags_query_full_true_contains_info_name(self, request_get_tags_query_full, tag_name):
+        assert tag_full_info_tag_name_provider(request_get_tags_query_full.data, tag_name), \
             f"Tag name {tag_name} is not present in full info"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_name, tag_description", tag_full_info_tag_descriptions)
-    def test_get_tags_query_full_true_contains_info_description(self, request_get_tags_query_full_true,
-                                                                tag_name, tag_description):
-        assert (tag_full_info_description_provider(request_get_tags_query_full_true.data, tag_name) ==
-                tag_description), \
+    def test_get_tags_query_full_true_contains_info_description(self,
+                                                                request_get_tags_query_full, tag_name, tag_description):
+        assert (tag_full_info_description_provider(request_get_tags_query_full.data, tag_name) == tag_description), \
             f"Tag's {tag_name}  tag description: {tag_description} is wrong"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_name, tag_id", tag_full_info_tag_ids)
-    def test_get_tags_query_full_true_contains_info_id(self, request_get_tags_query_full_true, tag_name, tag_id):
-        assert tag_full_info_id_provider(request_get_tags_query_full_true.data, tag_name) == tag_id, \
+    def test_get_tags_query_full_true_contains_info_id(self, request_get_tags_query_full, tag_name, tag_id):
+        assert tag_full_info_id_provider(request_get_tags_query_full.data, tag_name) == tag_id, \
             f"Tag's {tag_name}  tag id: {tag_id} is wrong"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_name, is_nsfw_state", tag_full_info_tag_is_nsfw_states)
-    def test_get_tags_query_full_true_contains_info_is_nsfw_state(self, request_get_tags_query_full_true,
-                                                                  tag_name, is_nsfw_state):
-        assert (tag_full_info_is_nsfw_state_provider(request_get_tags_query_full_true.data, tag_name) ==
+    def test_get_tags_query_full_true_contains_info_is_nsfw_state(self,
+                                                                  request_get_tags_query_full, tag_name, is_nsfw_state):
+        assert (tag_full_info_is_nsfw_state_provider(request_get_tags_query_full.data, tag_name) ==
                 is_nsfw_state), f"Tag's {tag_name}  is_nsfw state: {is_nsfw_state} is wrong"
 
     @pytest.mark.tags
     @pytest.mark.positive
     @pytest.mark.response_body
     @pytest.mark.parametrize("tag_full_info", tags_full_info)
-    def test_get_tags_query_full_true_contains_info(self, tag_full_info, request_get_tags_query_full_true):
-        assert tag_full_info_comparer(request_get_tags_query_full_true.data, tag_full_info), \
+    def test_get_tags_query_full_true_contains_info(self, tag_full_info, request_get_tags_query_full):
+        assert tag_full_info_comparer(request_get_tags_query_full.data, tag_full_info), \
             f"{tag_full_info['name']} tag has incorrect full info"
 
 
